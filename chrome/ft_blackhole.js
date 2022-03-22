f87343c7839843913ed5e9f95f2515fd760fcd90
Mohamed Haddi <mhaddi@student.1337.ma>
2022-03-22 19:51:57 +0100

    finish extension for chrome

--- chrome/ft_blackhole.js
+++ chrome/ft_blackhole.js
@@ -2,24 +2,45 @@ let blackholeDiv = document
     .getElementById("bh")
     .getElementsByClassName("emote-bh")[0];
 
-let blackholeDate = (() => {
-    let date = blackholeDiv.getAttribute("data-original-title");
-    let [day, month, year] = [...date.split("/")];
-    return `${month}/${day}/${year}`;
+/*
+ *
+ * In case `data-original-title`'s value was removed or changed, use this code calculate it:
+ *
+ * let blackholeDate = (() => {
+ *     let date = blackholeDiv.getAttribute("data-original-title");
+ *     let [day, month, year] = [...date.split("/")];
+ *     return `${month}/${day}/${year}`;
+ * })();
+ *
+ * let daysLeft = `${
+ *     Math.floor(
+ *          (new Date(blackholeDate) - new Date())
+ *          / (1000 * 60 * 60 * 24)
+ *     )
+ * } days left`;
+ *
+*/
+
+let daysLeft = blackholeDiv.getAttribute("data-original-title");
+let daysNum = daysLeft.split(" ")[0];
+
+let status = (() => {
+    if (daysNum <= 14)
+        return {cat: "😿", color: "#D8636F"};
+    else if (daysNum <= 42)
+        return {cat: "🙀", color: "#F0AD4E"};
+    else
+        return {cat: "😸", color: "#5CB85C"};
 })();
 
-let daysLeft = Math.floor(
-    (new Date(blackholeDate) - new Date())
-    / (1000 * 60 * 60 * 24)
-);
-
 blackholeDiv
-    .children[2]
+    .children[1]
     .appendChild((() => {
         let daysLeftDiv = document.createElement("div");
-        daysLeftDiv.innerText = `${daysLeft} days left`;
         daysLeftDiv.style.fontSize = "0.7em";
         daysLeftDiv.style.fontWeight = "400";
+        daysLeftDiv.innerText = daysLeft + ' ' + status["cat"];
+        daysLeftDiv.style.color = status["color"];
         return daysLeftDiv;
     })()
     );
