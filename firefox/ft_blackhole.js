11e605500b52d141809a01385d585f8c86ed484c
Mohamed Haddi <m.haddi.95@gmail.com>
2022-03-23 12:37:59 +0100

    add setTimeout to run script after dyanmic changes

--- firefox/ft_blackhole.js
+++ firefox/ft_blackhole.js
@@ -1,6 +1,31 @@
-let blackholeDiv = document
-    .getElementById("bh")
-    .getElementsByClassName("emote-bh")[0];
+setTimeout(function () {
+    const blackholeDiv = document
+        .getElementById("bh")
+        .getElementsByClassName("emote-bh")[0];
+
+    const daysLeft = blackholeDiv.getAttribute("data-original-title");
+    const daysNum = daysLeft.split(" ")[0];
+
+    const status = (() => {
+        if (daysNum <= 14)
+            return {cat: "😿", color: "#D8636F"};
+        else if (daysNum <= 42)
+            return {cat: "🙀", color: "#F0AD4E"};
+        else
+            return {cat: "😸", color: "#5CB85C"};
+    })();
+
+    const daysLeftDiv = document.createElement("div");
+    daysLeftDiv.innerText = daysLeft + ' ' + status["cat"];
+    daysLeftDiv.style.color = status["color"];
+    daysLeftDiv.style.fontSize = "0.7em";
+    daysLeftDiv.style.fontWeight = "400";
+    daysLeftDiv.style.animation = "0.42s ease 0s 1 normal none running fadeIn";
+
+    blackholeDiv
+        .children[1]
+        .appendChild(daysLeftDiv);
+}, 300);
 
 /*
  *
@@ -21,24 +46,24 @@ let blackholeDiv = document
  *
 */
 
-let daysLeft = blackholeDiv.getAttribute("data-original-title");
-let daysNum = daysLeft.split(" ")[0];
+/*
+ *
+ * Callback function to execute when mutations are observed:
+ *
+ * const callback = function (mutationsList, observer) {
+ *     for (const mutation of mutationsList) {
+ *         if (mutation.type === 'childList') {
+ *             console.log('A child node has been added or removed: ', mutation);
+ *         }
+ *         else if (mutation.type === 'attributes') {
+ *             console.log('The ' + mutation.attributeName + ' attribute was modified: ', mutation);
+ *         }
+ *     }
+ * };
+ *
+ * const config = {attributes: true, childList: true, subtree: true};
+ * const observer = new MutationObserver(callback);
+ * observer.observe(blackholeDiv, config);
+ *
+ */
 
-let status = (() => {
-    if (daysNum <= 14)
-        return {cat: "😿", color: "#D8636F"};
-    else if (daysNum <= 42)
-        return {cat: "🙀", color: "#F0AD4E"};
-    else
-        return {cat: "😸", color: "#5CB85C"};
-})();
-
-let daysLeftDiv = document.createElement("div");
-daysLeftDiv.innerText = daysLeft + ' ' + status["cat"];
-daysLeftDiv.style.color = status["color"];
-daysLeftDiv.style.fontSize = "0.7em";
-daysLeftDiv.style.fontWeight = "400";
-
-blackholeDiv
-    .children[1]
-    .appendChild(daysLeftDiv);
